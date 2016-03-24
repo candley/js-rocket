62688d5681f77a6995e79ccb4f6616e7fa12b0f3
candley <lnyandle@gmail.com>
2016-03-23 20:52:03 -0400

    Made my rocket!

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//hey this is invisible comment
+/*this comment is
+    multiple lines*/
+
+var doCoolStuff = function () {
+    var currentClassName = document.getElementById('cool').className;
+
+    if (currentClassName == 'cool') {
+        document.getElementById('cool').className = 'cool red';
+    } else{
+        document.getElementById('cool').className = 'cool';
+    }
+    
+
+}
+
+var sayMyName = function (name) {
+    alert('My name is: ' +name);
+}
+
+var  car = {
+        make: 'VW',
+        type: 'Polo',
+        color: 'blue',
+        isTurnedOn: false,
+        numberOfWheels: 4,
+        seats: ['seat1', 'seat2', 'seat3', 'seat4'],
+        turnOn: function (){
+            this.isTurnedOn = true;
+        },
+        fly: function (){
+            alert('fly');
+        },
+        switchCar: function (isOn){
+            console.log('turn car ' + isOn);
+            if (isOn == true) {
+                this.isTurnedOn = true;
+            } else {
+                this.isTurnedOn = false;
+            }
+        }
+    };
+
+console.log('hello there friends!');
